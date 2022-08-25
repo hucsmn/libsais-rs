73bfa3ae6143d7556ffabf1133f9bf55c38aea7e
hucsmn <hucsmn@hotmail.com>
2022-08-25 23:46:42 +0800

    define ffi from headers

--- sais-sys/src/sais16.rs
+++ sais-sys/src/sais16.rs
@@ -1,2 +1,81 @@
+use libc::c_void;
+
 extern "C" {
+    /// void * libsais16_create_ctx(void);
+    fn libsais16_create_ctx() -> *mut c_void;
+
+    /// void libsais16_free_ctx(void * ctx);
+    fn libsais16_free_ctx(ctx: *mut c_void);
+
+    /// int32_t libsais16(const uint16_t * T, int32_t * SA, int32_t n, int32_t fs, int32_t * freq);
+    fn libsais16(t: *const u16, sa: *mut i32, n: i32, fs: i32, freq: *const i32) -> i32;
+
+    /// int32_t libsais16_ctx(const void * ctx, const uint16_t * T, int32_t * SA, int32_t n, int32_t fs, int32_t * freq);
+    fn libsais16_ctx(ctx: *mut c_void, t: *const u16, sa: *mut i32, n: i32, fs: i32, freq: *const i32) -> i32;
+
+    /// int32_t libsais16_bwt(const uint16_t * T, uint16_t * U, int32_t * A, int32_t n, int32_t fs, int32_t * freq);
+    fn libsais16_bwt(t: *const u16, u: *mut u16, a: *mut i32, n: i32, fs: i32, freq: *const i32) -> i32;
+
+    /// int32_t libsais16_bwt_aux(const uint16_t * T, uint16_t * U, int32_t * A, int32_t n, int32_t fs, int32_t * freq, int32_t r, int32_t * I);
+    fn libsais16_bwt_aux(t: *const u16, u: *mut u16, a: *mut i32, n: i32, fs: i32, freq: *const i32, r: i32, i: *mut i32) -> i32;
+
+    /// int32_t libsais16_bwt_ctx(const void * ctx, const uint16_t * T, uint16_t * U, int32_t * A, int32_t n, int32_t fs, int32_t * freq);
+    fn libsais16_bwt_ctx(ctx: *mut c_void, t: *const u16, u: *mut u16, a: *mut i32, n: i32, fs: i32, freq: *const i32) -> i32;
+
+    /// int32_t libsais16_bwt_aux_ctx(const void * ctx, const uint16_t * T, uint16_t * U, int32_t * A, int32_t n, int32_t fs, int32_t * freq, int32_t r, int32_t * I);
+    fn libsais16_bwt_aux_ctx(ctx: *mut c_void, t: *const u16, u: *mut u16, a: *mut i32, n: i32, fs: i32, freq: *const i32, r: i32, i: *mut i32) -> i32;
+
+    /// void * libsais16_unbwt_create_ctx(void);
+    fn libsais16_unbwt_create_ctx() -> *mut c_void;
+
+    /// void libsais16_unbwt_free_ctx(void * ctx);
+    fn libsais16_unbwt_free_ctx(ctx: *mut c_void);
+
+    /// int32_t libsais16_unbwt(const uint16_t * T, uint16_t * U, int32_t * A, int32_t n, const int32_t * freq, int32_t i);
+    fn libsais16_unbwt(t: *const u16, u: *mut u16, a: *mut i32, n: i32, freq: *const i32, i: i32) -> i32;
+
+    /// int32_t libsais16_unbwt_ctx(const void * ctx, const uint16_t * T, uint16_t * U, int32_t * A, int32_t n, const int32_t * freq, int32_t i);
+    fn libsais16_unbwt_ctx(ctx: *mut c_void, t: *const u16, u: *mut u16, a: *mut i32, n: i32, freq: *const i32, i: i32) -> i32;
+
+    /// int32_t libsais16_unbwt_aux(const uint16_t * T, uint16_t * U, int32_t * A, int32_t n, const int32_t * freq, int32_t r, const int32_t * I);
+    fn libsais16_unbwt_aux(t: *const u16, u: *mut u16, a: *mut i32, n: i32, freq: *const i32, r: i32, i: *mut i32) -> i32;
+
+    /// int32_t libsais16_unbwt_aux_ctx(const void * ctx, const uint16_t * T, uint16_t * U, int32_t * A, int32_t n, const int32_t * freq, int32_t r, const int32_t * I);
+    fn libsais16_unbwt_aux_ctx(ctx: *mut c_void, t: *const u16, u: *mut u16, a: *mut i32, n: i32, freq: *const i32, r: i32, i: *mut i32) -> i32;
+
+    /// int32_t libsais16_plcp(const uint16_t * T, const int32_t * SA, int32_t * PLCP, int32_t n);
+    fn libsais16_plcp(t: *const u16, sa: *const i32, plcp: *mut i32, n: i32) -> i32;
+
+    /// int32_t libsais16_lcp(const int32_t * PLCP, const int32_t * SA, int32_t * LCP, int32_t n);
+    fn libsais16_lcp(plcp: *const u16, sa: *const i32, lcp: *mut i32, n: i32) -> i32;
+}
+
+#[cfg(feature = "openmp")]
+extern "C" {
+    /// void * libsais16_create_ctx_omp(int32_t threads);
+    fn libsais16_create_ctx_omp(threads: i32) -> *mut c_void;
+
+    /// int32_t libsais16_omp(const uint16_t * T, int32_t * SA, int32_t n, int32_t fs, int32_t * freq, int32_t threads);
+    fn libsais16_omp(t: *const u16, sa: *mut i32, n: i32, fs: i32, freq: *const i32, threads: i32) -> i32;
+
+    /// int32_t libsais16_bwt_omp(const uint16_t * T, uint16_t * U, int32_t * A, int32_t n, int32_t fs, int32_t * freq, int32_t threads);
+    fn libsais16_bwt_omp(t: *const u16, u: *mut u16, a: *mut i32, n: i32, fs: i32, freq: *const i32, threads: i32) -> i32;
+
+    /// int32_t libsais16_bwt_aux_omp(const uint16_t * T, uint16_t * U, int32_t * A, int32_t n, int32_t fs, int32_t * freq, int32_t r, int32_t * I, int32_t threads);
+    fn libsais16_bwt_aux_omp(t: *const u16, u: *mut u16, a: *mut i32, n: i32, fs: i32, freq: *const i32, r: i32, i: *mut i32, threads: i32) -> i32;
+
+    /// void * libsais16_unbwt_create_ctx_omp(int32_t threads);
+    fn libsais16_unbwt_create_ctx_omp(threads: i32) -> *mut c_void;
+
+    /// int32_t libsais16_unbwt_omp(const uint16_t * T, uint16_t * U, int32_t * A, int32_t n, const int32_t * freq, int32_t i, int32_t threads);
+    fn libsais16_unbwt_omp(t: *const u16, u: *mut u16, a: *mut i32, n: i32, freq: *const i32, i: i32, threads: i32) -> i32;
+
+    /// int32_t libsais16_unbwt_aux_omp(const uint16_t * T, uint16_t * U, int32_t * A, int32_t n, const int32_t * freq, int32_t r, const int32_t * I, int32_t threads);
+    fn libsais16_unbwt_aux_omp(t: *const u16, u: *mut u16, a: *mut i32, n: i32, freq: *const i32, r: i32, i: *mut i32, threads: i32) -> i32;
+
+    /// int32_t libsais16_plcp_omp(const uint16_t * T, const int32_t * SA, int32_t * PLCP, int32_t n, int32_t threads);
+    fn libsais16_plcp_omp(t: *const u16, sa: *const i32, plcp: *mut i32, n: i32, threads: i32) -> i32;
+
+    /// int32_t libsais16_lcp_omp(const int32_t * PLCP, const int32_t * SA, int32_t * LCP, int32_t n, int32_t threads);
+    fn libsais16_lcp_omp(plcp: *const u16, sa: *const i32, lcp: *mut i32, n: i32, threads: i32) -> i32;
 }
--- sais-sys/src/sais64.rs
+++ sais-sys/src/sais64.rs
@@ -1,2 +1,46 @@
 extern "C" {
+    /// int64_t libsais64(const uint8_t * T, int64_t * SA, int64_t n, int64_t fs, int64_t * freq);
+    fn libsais64(t: *const u8, sa: *mut i64, n: i64, fs: i64, freq: *mut i64) -> i64;
+
+    /// int64_t libsais64_bwt(const uint8_t * T, uint8_t * U, int64_t * A, int64_t n, int64_t fs, int64_t * freq);
+    fn libsais64_bwt(t: *const u8, u: *mut u8, a: *mut i64, n: i64, fs: i64, freq: *mut i64) -> i64;
+
+    /// int64_t libsais64_bwt_aux(const uint8_t * T, uint8_t * U, int64_t * A, int64_t n, int64_t fs, int64_t * freq, int64_t r, int64_t * I);
+    fn libsais64_bwt_aux(t: *const u8, u: *mut u8, a: *mut i64, n: i64, fs: i64, freq: *mut i64, r: i64, i: *mut i64) -> i64;
+
+    /// int64_t libsais64_unbwt(const uint8_t * T, uint8_t * U, int64_t * A, int64_t n, const int64_t * freq, int64_t i);
+    fn libsais64_unbwt(t: *const u8, u: *mut u8, a: *mut i64, n: i64, freq: *mut i64, i: i64) -> i64;
+
+    /// int64_t libsais64_unbwt_aux(const uint8_t * T, uint8_t * U, int64_t * A, int64_t n, const int64_t * freq, int64_t r, const int64_t * I);
+    fn libsais64_unbwt_aux(t: *const u8, u: *mut u8, a: *mut i64, n: i64, freq: *mut i64, r: i64, i: *mut i64) -> i64;
+
+    /// int64_t libsais64_plcp(const uint8_t * T, const int64_t * SA, int64_t * PLCP, int64_t n);
+    fn libsais64_plcp(t: *const u8, sa: *const i64, plcp: *mut i64, n: i64) -> i64;
+
+    /// int64_t libsais64_lcp(const int64_t * PLCP, const int64_t * SA, int64_t * LCP, int64_t n);
+    fn libsais64_lcp(plcp: *const u8, sa: *const i64, lcp: *mut i64, n: i64) -> i64;
+}
+
+#[cfg(feature = "openmp")]
+extern "C" {
+    /// int64_t libsais64_omp(const uint8_t * T, int64_t * SA, int64_t n, int64_t fs, int64_t * freq, int64_t threads);
+    fn libsais64_omp(t: *const u8, sa: *mut i64, n: i64, fs: i64, freq: *mut i64, threads: i64) -> i64;
+
+    /// int64_t libsais64_bwt_omp(const uint8_t * T, uint8_t * U, int64_t * A, int64_t n, int64_t fs, int64_t * freq, int64_t threads);
+    fn libsais64_bwt_omp(t: *const u8, u: *mut u8, a: *mut i64, n: i64, fs: i64, freq: *mut i64, threads: i64) -> i64;
+
+    /// int64_t libsais64_bwt_aux_omp(const uint8_t * T, uint8_t * U, int64_t * A, int64_t n, int64_t fs, int64_t * freq, int64_t r, int64_t * I, int64_t threads);
+    fn libsais64_bwt_aux_omp(t: *const u8, u: *mut u8, a: *mut i64, n: i64, fs: i64, freq: *mut i64, r: i64, i: *mut i64, threads: i64) -> i64;
+
+    /// int64_t libsais64_unbwt_omp(const uint8_t * T, uint8_t * U, int64_t * A, int64_t n, const int64_t * freq, int64_t i, int64_t threads);
+    fn libsais64_unbwt_omp(t: *const u8, u: *mut u8, a: *mut i64, n: i64, freq: *mut i64, i: i64, threads: i64) -> i64;
+
+    /// int64_t libsais64_unbwt_aux_omp(const uint8_t * T, uint8_t * U, int64_t * A, int64_t n, const int64_t * freq, int64_t r, const int64_t * I, int64_t threads);
+    fn libsais64_unbwt_aux_omp(t: *const u8, u: *mut u8, a: *mut i64, n: i64, freq: *mut i64, r: i64, i: *mut i64, threads: i64) -> i64;
+
+    /// int64_t libsais64_plcp_omp(const uint8_t * T, const int64_t * SA, int64_t * PLCP, int64_t n, int64_t threads);
+    fn libsais64_plcp_omp(t: *const u8, sa: *const i64, plcp: *mut i64, n: i64, threads: i64) -> i64;
+
+    /// int64_t libsais64_lcp_omp(const int64_t * PLCP, const int64_t * SA, int64_t * LCP, int64_t n, int64_t threads);
+    fn libsais64_lcp_omp(plcp: *const u8, sa: *const i64, lcp: *mut i64, n: i64, threads: i64) -> i64;
 }
